885ccaf2f5eed94fb428824915d59de35887fb94
StarkovSemen <semastarkov@gmail.com>
2026-06-17 00:18:14 +0500

    Update keywords.cs

--- ConsoleApp1/ConsoleApp1/keywords.cs
+++ ConsoleApp1/ConsoleApp1/keywords.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace Компилятор
 {
-    class Keywords
+    class KeyWords
     {
-        public Dictionary<byte, Dictionary<string, byte>> Kw { get; } = new Dictionary<byte, Dictionary<string, byte>>
+        public Dictionary<byte, Dictionary<string, byte>> Kw { get; }
+
+        public KeyWords()
         {
-            [2] = new Dictionary<string, byte>
+            Kw = new Dictionary<byte, Dictionary<string, byte>>();
+
+            var kw2 = new Dictionary<string, byte>
             {
                 ["do"] = LexicalAnalyzer.dosy,
                 ["if"] = LexicalAnalyzer.ifsy,
@@ -14,8 +18,10 @@ namespace Компилятор
                 ["of"] = LexicalAnalyzer.ofsy,
                 ["or"] = LexicalAnalyzer.orsy,
                 ["to"] = LexicalAnalyzer.tosy,
-            },
-            [3] = new Dictionary<string, byte>
+            };
+            Kw.Add((byte)2, kw2);
+
+            var kw3 = new Dictionary<string, byte>
             {
                 ["end"] = LexicalAnalyzer.endsy,
                 ["var"] = LexicalAnalyzer.varsy,
@@ -26,8 +32,10 @@ namespace Компилятор
                 ["mod"] = LexicalAnalyzer.modsy,
                 ["nil"] = LexicalAnalyzer.nilsy,
                 ["set"] = LexicalAnalyzer.setsy,
-            },
-            [4] = new Dictionary<string, byte>
+            };
+            Kw.Add((byte)3, kw3);
+
+            var kw4 = new Dictionary<string, byte>
             {
                 ["then"] = LexicalAnalyzer.thensy,
                 ["else"] = LexicalAnalyzer.elsesy,
@@ -36,8 +44,10 @@ namespace Компилятор
                 ["goto"] = LexicalAnalyzer.gotosy,
                 ["type"] = LexicalAnalyzer.typesy,
                 ["with"] = LexicalAnalyzer.withsy,
-            },
-            [5] = new Dictionary<string, byte>
+            };
+            Kw.Add((byte)4, kw4);
+
+            var kw5 = new Dictionary<string, byte>
             {
                 ["begin"] = LexicalAnalyzer.beginsy,
                 ["while"] = LexicalAnalyzer.whilesy,
@@ -45,17 +55,26 @@ namespace Компилятор
                 ["const"] = LexicalAnalyzer.constsy,
                 ["label"] = LexicalAnalyzer.labelsy,
                 ["until"] = LexicalAnalyzer.untilsy,
-            },
-            [6] = new Dictionary<string, byte>
+            };
+            Kw.Add((byte)5, kw5);
+
+            var kw6 = new Dictionary<string, byte>
             {
                 ["downto"] = LexicalAnalyzer.downtosy,
                 ["packed"] = LexicalAnalyzer.packedsy,
                 ["record"] = LexicalAnalyzer.recordsy,
                 ["repeat"] = LexicalAnalyzer.repeatsy,
-            },
-            [7] = new Dictionary<string, byte> { ["program"] = LexicalAnalyzer.programsy },
-            [8] = new Dictionary<string, byte> { ["function"] = LexicalAnalyzer.functionsy },
-            [9] = new Dictionary<string, byte> { ["procedure"] = LexicalAnalyzer.procedurensy },
-        };
+            };
+            Kw.Add((byte)6, kw6);
+
+            var kw7 = new Dictionary<string, byte> { ["program"] = LexicalAnalyzer.programsy };
+            Kw.Add((byte)7, kw7);
+
+            var kw8 = new Dictionary<string, byte> { ["function"] = LexicalAnalyzer.functionsy };
+            Kw.Add((byte)8, kw8);
+
+            var kw9 = new Dictionary<string, byte> { ["procedure"] = LexicalAnalyzer.procedurensy };
+            Kw.Add((byte)9, kw9);
+        }
     }
 }
